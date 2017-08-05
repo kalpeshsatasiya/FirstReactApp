58bea1f1d48f3bdc18d3e4e6a86eab47a5b448df
kalpeshsatasiya <satasiya.kalpesh2006@gmail.com>
2017-08-05 12:10:39 +0530

    first commit

---
+++ src/First.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class First extends Component {
+  render() {
+    return (
+      <div >
+       <span style={{color: 'blue'}}> This is the first component</span>
+      </div>
+    );
+  }
+}
+
+export default First;
